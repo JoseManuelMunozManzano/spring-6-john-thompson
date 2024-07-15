4a10a914b231600d37f2ff84b3b0ff7778eff235
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2024-07-15 20:20:21 +0200

    DI - Test de Contexto de Spring

--- spring-6-di/README.md
+++ spring-6-di/README.md
@@ -7,10 +7,17 @@ Vamos a construir una pequeña aplicación web usando Spring Boot para enseñar 
 - Spring context
   - ApplicationContext ctx = SpringApplication.run(DiApplication.class, args);
   - MyController controller = ctx.getBean(MyController.class);
+- Spring context en contextos de test
+  - @Autowired ApplicationContext applicationContext;
 
 ## Testing
 
 - Clonar el repositorio
-- Ejecutar la aplicación
+- App
+  - Ejecutar la aplicación
+- Tests
+  - Ejecutar los dos tests de la aplicación de manera independiente
+    - testAutowireOfController()
+    - testGetControllerFromCtx()
 
 Notar que el programa se ejecuta y termina porque no hay servidor Tomcat interno en el POM.
--- spring-6-di/src/test/java/com/jmunoz/di/DiApplicationTests.java
+++ spring-6-di/src/test/java/com/jmunoz/di/DiApplicationTests.java
@@ -1,10 +1,38 @@
 package com.jmunoz.di;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
+import com.jmunoz.di.controllers.MyController;
+
+// Esta anotación indica al test que cree el contexto de Spring.
 @SpringBootTest
 class DiApplicationTests {
+
+	// Spring context inyecta el contexto gracias a @Autowired
+	@Autowired
+	ApplicationContext applicationContext;
+
+	// Segunda forma de obtener la instancia del controlador, usando inyección del controlador
+	// con @Autowired.
+	@Autowired
+	MyController myController;
+
+	@Test
+	void testAutowireOfController() {
+		// Utilización del controller con la segunda forma de instanciarlo.
+		System.out.println(myController.sayHello());
+	}
+
+	@Test
+	void testGetControllerFromCtx() {
+		// Primera forma de obtener la instancia del controlador usando el contexto de Spring, y utilización.
+		MyController myController = applicationContext.getBean(MyController.class);
+
+		System.out.println(myController.sayHello());
+	}
 
 	@Test
 	void contextLoads() {
