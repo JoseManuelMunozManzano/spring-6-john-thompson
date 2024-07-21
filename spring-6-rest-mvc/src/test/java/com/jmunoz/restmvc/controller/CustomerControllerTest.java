80a369917d56ba84c3eeee4fd84e27670b38f07b
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2024-07-21 07:10:37 +0200

    Mockito - MockMVC Test Update Customer

--- spring-6-rest-mvc/src/test/java/com/jmunoz/restmvc/controller/CustomerControllerTest.java
+++ spring-6-rest-mvc/src/test/java/com/jmunoz/restmvc/controller/CustomerControllerTest.java
@@ -19,8 +19,8 @@ import java.util.UUID;
 import static org.hamcrest.core.Is.is;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(CustomerController.class)
@@ -103,5 +103,19 @@ class CustomerControllerTest {
                 .content(objectMapper.writeValueAsString(customer)))
                 .andExpect(status().isCreated())
                 .andExpect(header().exists("Location"));
+    }
+
+    @Test
+    void testUpdateCustomer() throws Exception {
+        Customer customer = testCustomers.getFirst();
+
+        mockMvc.perform(put("/api/v1/customer/" + customer.getId())
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(customer)))
+                .andExpect(status().isNoContent());
+
+        // En este caso, en vez de usar eq(customer.getId()) he usado any
+        verify(customerService).updateCustomerById(any(UUID.class), any(Customer.class));
     }
 }
