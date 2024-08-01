fd61af57dfec9a06c0d24663943d3d34edc70c15
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2024-08-01 18:30:32 +0200

    MySQL - Console Logging of SQL Statements and Fix Error Id with no length in Varchar

--- spring-6-mysql/src/main/java/com/jmunoz/restmvc/entities/CustomerEntity.java
+++ spring-6-mysql/src/main/java/com/jmunoz/restmvc/entities/CustomerEntity.java
@@ -1,9 +1,6 @@
 package com.jmunoz.restmvc.entities;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Version;
+import jakarta.persistence.*;
 import lombok.*;
 import org.hibernate.annotations.UuidGenerator;
 
@@ -17,6 +14,7 @@ import java.util.UUID;
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
+@Table(name="customer")
 public class CustomerEntity {
 
     // Dada esta configuración necesitamos hacer un mapeo de BBDD, de ahí el uso de @Column.
