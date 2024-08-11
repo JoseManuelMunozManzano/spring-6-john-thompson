539f8a8396779d7d4c319e5419a2ba93e9bda948
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2024-08-11 06:00:48 +0200

    DB Relationship Mappings - Persisting Beer Order Relationships

--- spring-6-db-relationships/src/test/java/com/jmunoz/restmvc/repositories/BeerOrderRepositoryTest.java
+++ spring-6-db-relationships/src/test/java/com/jmunoz/restmvc/repositories/BeerOrderRepositoryTest.java
@@ -1,11 +1,13 @@
 package com.jmunoz.restmvc.repositories;
 
 import com.jmunoz.restmvc.entities.BeerEntity;
+import com.jmunoz.restmvc.entities.BeerOrderEntity;
 import com.jmunoz.restmvc.entities.CustomerEntity;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -37,19 +39,38 @@ class BeerOrderRepositoryTest {
     void setUp() {
         // Esta es la data que queremos usar para nuestros tests.
         // También podríamos crear data más concreta, pero vamos a usar el primer registro de nuestra BD.
-        // Con esto compondremos una BeerOrderEntity.
         testCustomer = customerRepository.findAll().getFirst();
         testBeer = beerRepository.findAll().getFirst();
     }
 
+    @Transactional
     @Test
     void testBeerOrders() {
         // Puede (@SopringBootTest) o no (@DataJpaTest) haber data en BD en función de la anotación de clase.
-        System.out.println(beerOrderRepository.count());
-        System.out.println(customerRepository.count());
-        System.out.println(beerRepository.count());
+        //
+        // System.out.println(beerOrderRepository.count());
+        // System.out.println(customerRepository.count());
+        // System.out.println(beerRepository.count());
+        // System.out.println(testCustomer.getName());
+        // System.out.println(testBeer.getBeerName());
 
-        System.out.println(testCustomer.getName());
-        System.out.println(testBeer.getBeerName());
+        // Componemos una BeerOrderEntity
+        BeerOrderEntity beerOrder = BeerOrderEntity.builder()
+                .customerRef("Test order")
+                .customer(testCustomer)
+                .build();
+
+        // Importante hacer el flush para que Hibernate persista inmediatamente la data a la BD.
+        // Si no se indica, la data se queda en Hibernate, lo que hace que el test pueda fallar.
+        //
+        // Esto es también porque en un contexto transaccional se hace una carga perezosa (lazy load)
+        // y estamos preguntando, dentro de customer, por beerOrders, que contendrá el elemento que hemos compuesto, y es lazy load.
+        // De nuevo, al hacer el flush, se persiste to-do a BD y obtenemos la relación bidireccional entre ambas entidades.
+        //
+        // A partir de ahora, en el test, se debe usar el objeto devuelto por el repositorio porque no se garantiza
+        // que al hacer el save() se devuelva la misma referencia.
+        BeerOrderEntity savedBeerOrder = beerOrderRepository.saveAndFlush(beerOrder);
+
+        System.out.println(savedBeerOrder.getCustomerRef());
     }
 }
