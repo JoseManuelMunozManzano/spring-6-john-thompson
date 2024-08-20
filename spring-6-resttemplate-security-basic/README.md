ad20f8222dde3ec12b5c154515c31a3ffcaf9195
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2024-08-20 19:56:14 +0200

    Spring Security Basic - Refactor of RestTemplate Builder Config

--- spring-6-resttemplate-security-basic/README.md
+++ spring-6-resttemplate-security-basic/README.md
@@ -7,6 +7,7 @@ Es el mismo proyecto `spring-6-resttemplate` al que se le ha añadido Http Basic
 ## Testing
 
 - Clonar el repositorio
+- Renombrar `application.template.properties` a `application.properties` e indicar sus valores
 - Ejecutar el proyecto `spring-6-security-basic` que será nuestro programa de backend que tiene seguridad
   - Se ejecuta en el puerto 8080
 - Ejecutar los tests de este proyecto, que será el cliente y llamará a los endpoints del proyecto backend
--- spring-6-resttemplate-security-basic/src/main/java/guru/springframework/spring6resttemplate/config/RestTemplateBuilderConfig.java
+++ spring-6-resttemplate-security-basic/src/main/java/guru/springframework/spring6resttemplate/config/RestTemplateBuilderConfig.java
@@ -29,23 +29,39 @@ public class RestTemplateBuilderConfig {
 
         // Para asegurarnos de que la propiedad está cargada.
         // Si no lo está fallará al comenzar la ejecución.
-        assert rootUrl != null;
+        //
+        // assert rootUrl != null;
 
         // Lo que hace configurer es coger la nueva instancia y la configura usando los valores por defecto
         // de SpringBoot.
-        RestTemplateBuilder builder = configurer.configure(new RestTemplateBuilder());
+        //
+        // RestTemplateBuilder builder = configurer.configure(new RestTemplateBuilder());
 
         // Luego, modificamos lo que necesitamos.
-        DefaultUriBuilderFactory uriBuilderFactory = new DefaultUriBuilderFactory(rootUrl);
+        //
+        // DefaultUriBuilderFactory uriBuilderFactory = new DefaultUriBuilderFactory(rootUrl);
 
         // Añadimos HTTP Basic Authentication para añadir la seguridad y que la llamada a nuestros endpoints
         // del proyecto de backend funcionen correctamente.
         // No olvidar cada vez que llamamos a builder.<lo que sea> se crea un nuevo builder. Tenemos que
         // recuperar esa instancia en una variable para que funcione correctamente.
-        RestTemplateBuilder builderWithAuth = builder.basicAuthentication(username, password);
+        //
+        // RestTemplateBuilder builderWithAuth = builder.basicAuthentication(username, password);
 
         // Importante devolver el builderWithAuth llamando al handler del uri para devolver todos los valores
         // del builder incluido to-do lo añadido, como la seguridad.
-        return builderWithAuth.uriTemplateHandler(uriBuilderFactory);
+        //
+        // return builderWithAuth.uriTemplateHandler(uriBuilderFactory);
+
+        // Refactorizamos a:
+        assert rootUrl != null;
+
+        RestTemplateBuilder builder = new RestTemplateBuilder();
+
+        DefaultUriBuilderFactory uriBuilderFactory = new DefaultUriBuilderFactory(rootUrl);
+
+        return configurer.configure(builder)
+                .basicAuthentication(username, password)
+                .uriTemplateHandler(uriBuilderFactory);
     }
 }
