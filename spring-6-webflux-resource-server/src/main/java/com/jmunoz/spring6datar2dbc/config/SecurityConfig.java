b0d56aadd1ed273bf05711c3d2cb481c87e52c37
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2024-10-04 18:24:55 +0200

    WebFlux Resource Server - Spring Security Configuration

---
+++ spring-6-webflux-resource-server/src/main/java/com/jmunoz/spring6datar2dbc/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.jmunoz.spring6datar2dbc.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+// Con esta configuración (ver también application.properties) configuramos nuestra aplicación
+// para ser un recurso OAuth2 server.
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+    @Bean
+    SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+
+        // Securizamos todos los endpoints
+        http.authorizeExchange(authorize -> authorize.anyExchange().authenticated())
+                .oauth2ResourceServer(resourceServer -> resourceServer.jwt(Customizer.withDefaults()));
+
+        return http.build();
+
+    }
+}
