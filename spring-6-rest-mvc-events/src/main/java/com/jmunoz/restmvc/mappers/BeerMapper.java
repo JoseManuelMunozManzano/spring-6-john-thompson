a9b4315b5a7190df1fbcbad7c465882170e94e1c
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2024-10-22 20:14:03 +0200

    Spring Application Events - Add Audit Mapper

--- spring-6-rest-mvc-events/src/main/java/com/jmunoz/restmvc/mappers/BeerMapper.java
+++ spring-6-rest-mvc-events/src/main/java/com/jmunoz/restmvc/mappers/BeerMapper.java
@@ -1,13 +1,25 @@
 package com.jmunoz.restmvc.mappers;
 
+import com.jmunoz.restmvc.entities.BeerAuditEntity;
 import com.jmunoz.restmvc.entities.BeerEntity;
 import com.jmunoz.restmvc.model.BeerDto;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper
 public interface BeerMapper {
 
+    @Mapping(target = "categories", ignore = true)
+    @Mapping(target = "beerOrderLines", ignore = true)
     BeerEntity beerDtoToBeerEntity(BeerDto dto);
 
     BeerDto beerEntityToBeerDto(BeerEntity beer);
+
+    // Añadimos los mappers para BeerAuditEntity e ignoramos las propiedades indicadas
+    // para que de verdad se pueda hacer el mapper entre ambas clases.
+    @Mapping(target = "createdDateAudit", ignore = true)
+    @Mapping(target = "auditId", ignore = true)
+    @Mapping(target = "auditEventType", ignore = true)
+    @Mapping(target = "principalName", ignore = true)
+    BeerAuditEntity beerEntityToBeerAuditEntity(BeerEntity beer);
 }
